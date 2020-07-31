2349bda6fbb01ad41824a8867c075f2be45075e2
Aarchu-B <archanabaral2000@gmail.com>
2020-07-31 13:12:49 +0545

    define database types

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
-import { Database } from "../lib/types";
+import { Database, User, Listing, Booking } from "../lib/types";
 
-const url = `${process.env.MONGO_URI}`
+const url = `${process.env.MONGO_URI}`;
 
 //our this function is async so we cannot simply specify return type of the value we expect, so we have to say it is going to be promise of return type
 export const connectdb = async (): Promise<Database> => {
@@ -11,10 +11,12 @@ export const connectdb = async (): Promise<Database> => {
   });
   console.log("Mongo DB connected");
 
-  //retriving adatabase and assigning to a variabale db
+  //retriving a database and assigning to a variabale db
   const db = client.db("main");
 
   return {
-    listings: db.collection("test_listings"),
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
   };
 };
--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,20 +1,55 @@
 import { ObjectId, Collection } from "mongodb";
 
+export interface Booking {
+  _id: ObjectId;
+  listing: ObjectId; //one-one
+  tenant: string; //one-one with user
+  checkIn: string;
+  checkOut: string;
+}
+
+export enum ListingType {
+  Apartment = "APARTMENT", //seting Apartment property with a string value of "APARTMENT"
+  House = "HOUSE",
+}
+export interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+export interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
+}
 export interface Listing {
   _id: ObjectId;
   title: string;
+  description: string;
   image: string;
+  host: string; //or owner //one to one
+  type: ListingType;
   address: string;
+  admin: string;
+  country: string;
+  city: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndex;
   price: number;
   numOfGuests: number;
-  numOfBeds: number;
-  numOfBaths: number;
-  rating: number;
 }
-
+export interface User {
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string; //to store user payments detail it will be of type string or undefine
+  income: number;
+  bookings: ObjectId[]; //one user has many bookings one to many relation
+  listings: ObjectId[]; //same
+}
 export interface Database {
-    //Collection is a generic and accepts a typed variable  adding listing interface we have created as a typed parameter of this collection interface
+  bookings: Collection<Booking>;
   listings: Collection<Listing>;
+  users: Collection<User>;
 }
-
-
