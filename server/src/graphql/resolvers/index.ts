59d26f12d058e9e3297f72fc654d304546a4aa02
Aarchu-B <archanabaral2000@gmail.com>
2020-08-12 13:25:08 +0545

    create Booking Resolver map

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -2,4 +2,10 @@ import merge from "lodash.merge";
 import { userResolver } from "./User";
 import { viewerResolvers } from "./Viewer";
 import { listingResolvers } from "./Listing";
-export const resolvers = merge(viewerResolvers, userResolver, listingResolvers);
+import { bookingResolvers } from "./Booking";
+export const resolvers = merge(
+  listingResolvers,
+  bookingResolvers,
+  viewerResolvers,
+  userResolver
+);
