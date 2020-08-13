bb16131d7c19b2ecd765c541d417b53bbbb91d3c
Aarchu-B <archanabaral2000@gmail.com>
2020-08-13 10:50:13 +0545

    Add dollar sign to ptice

--- client/src/lib/utils/index.ts
+++ client/src/lib/utils/index.ts
@@ -1,20 +1,25 @@
-import {message, notification} from "antd"
+import { message, notification } from "antd";
 
-export const displaySuccessNotification = (message:string, description?:string)=>{
+export const iconColor = "#1890ff";
 
-    return notification["success"](
-        {
-            message,
-            description,
-            placement:"topLeft",
-            style:{
-                marginTop:50
-            }
-        }
-    )
+export const formatListingPrice = (price: number, round: true) => {
+  const formattedListingPrice = round ? Math.round(price / 100) : price / 100;
+  return `$${formattedListingPrice}`;
+};
+export const displaySuccessNotification = (
+  message: string,
+  description?: string
+) => {
+  return notification["success"]({
+    message,
+    description,
+    placement: "topLeft",
+    style: {
+      marginTop: 50,
+    },
+  });
+};
 
-}
-
-export const displayErrorMessage = (error:string) => {
-    return message.error(error)
-}
+export const displayErrorMessage = (error: string) => {
+  return message.error(error);
+};
