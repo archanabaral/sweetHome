2fb64c35bf33f3923fa9f55123684f09786f9162
Aarchu-B <archanabaral2000@gmail.com>
2020-08-26 14:39:00 +0545

    add listings field to query

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -7,6 +7,11 @@ export const typeDefs = gql`
     APARTMENT
     HOUSE
   }
+
+  enum ListingFilter {
+    PRICE_LOW_TO_HIGH
+    PRICE_HIGH_TO_LOW
+  }
   type Listing {
     id: ID!
     title: String!
@@ -64,6 +69,7 @@ export const typeDefs = gql`
     authUrl: String!
     user(id: ID!): User!
     listing(id: ID!): Listing!
+    listings(filter:ListingFilter! , limit:Int!, page:Int!):Listings!
   }
 
   type Mutation {
